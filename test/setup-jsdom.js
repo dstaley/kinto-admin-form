48325a98ed58eb77414500d2999747d79f548fcf
Dylan Staley <88163+dstaley@users.noreply.github.com>
2020-10-09 12:59:21 -0700

    update to latest version of react

--- test/setup-jsdom.js
+++ test/setup-jsdom.js
@@ -1,10 +1,14 @@
+require("raf/polyfill");
 var jsdom = require("jsdom");
 
 // Setup the jsdom environment
 // @see https://github.com/facebook/react/issues/5046
 if (!global.hasOwnProperty("window")) {
-  global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
-  global.window = document.defaultView;
+  const dom = new jsdom.JSDOM("<!doctype html><html><body></body></html>", {
+    url: "http://localhost",
+  });
+  global.window = dom.window;
+  global.document = dom.window.document;
   global.navigator = global.window.navigator;
   global.File = global.window.File;
 }
